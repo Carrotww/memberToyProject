43e9ae7d8043cb9ca7d2269ac93379665a1fc523
Carrot_hyeong <rjgjfl90@naver.com>
2024-04-26 00:53:21 +0900

    feat : security 설정 config 및 password encoder bean 등록

---
+++ src/main/java/com/commerce/toy/global/configuration/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.commerce.toy.global.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String[] WHITELIST = {
+            "/api/user/",
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers(WHITELIST).permitAll()
+            .anyRequest().authenticated();
+    }
+}
