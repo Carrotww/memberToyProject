311f36ca97efecbec1308c90f4e7ea4c4084309b
Carrot_hyeong <rjgjfl90@naver.com>
2024-04-26 01:11:41 +0900

    fix : security 설정 수정

--- src/main/java/com/commerce/toy/global/configuration/SecurityConfig.java
+++ src/main/java/com/commerce/toy/global/configuration/SecurityConfig.java
@@ -5,14 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     private static final String[] WHITELIST = {
             "/api/user/",
     };
@@ -22,12 +24,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers(WHITELIST).permitAll()
-            .anyRequest().authenticated();
+                .csrf().disable()
+                .authorizeRequests(request -> request
+                    .antMatchers(WHITELIST).permitAll()
+                    .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }
