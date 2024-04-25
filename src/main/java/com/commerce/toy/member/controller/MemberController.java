54778e27489fd83695dede44ca22252fe7448073
Carrot_hyeong <rjgjfl90@naver.com>
2024-04-25 22:51:14 +0900

    feat : member controller api init

---
+++ src/main/java/com/commerce/toy/member/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.commerce.toy.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class MemberController {
+
+    @PostMapping("/join")
+    public ResponseEntity<Void> memberRegister() {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Void> getMembers(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "memberName, asc") String sort) {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{memberId}")
+    public ResponseEntity<Void> updateMember(@PathVariable("memberId") Long memberId) {
+
+        return ResponseEntity.ok().build();
+    }
+}
