484dc7ecbcc73bed7741047264e71e0ddf49bd9c
Carrot_hyeong <rjgjfl90@naver.com>
2024-04-27 00:23:43 +0900

    feat : yml 파일 추가 및 security 로그인 폼 해제

--- src/main/java/com/commerce/toy/global/configuration/SecurityConfig.java
+++ src/main/java/com/commerce/toy/global/configuration/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.commerce.toy.global.configuration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,7 +21,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private static final String[] WHITELIST = {
-            "/api/user/",
+            "/api/user/**",
+            "/h2-console/**",
     };
 
     @Bean
@@ -27,11 +33,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests(request -> request
-                    .antMatchers(WHITELIST).permitAll()
-                    .anyRequest().authenticated())
-                .httpBasic(withDefaults());
+                        .antMatchers(WHITELIST).permitAll())
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .headers().frameOptions().disable();
         return http.build();
     }
 }
