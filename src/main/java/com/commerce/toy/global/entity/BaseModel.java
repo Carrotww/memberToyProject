21dbf80ba867a0acbb4984519a1b246462ee249c
Carrotww <rjgjfl90@naver.com>
2024-04-28 17:58:40 +0900

    feat : BaseModel 추가

--- src/main/java/com/commerce/toy/global/entity/BaseModel.java
+++ src/main/java/com/commerce/toy/global/entity/BaseModel.java
@@ -1,5 +1,7 @@
 package com.commerce.toy.global.entity;
 
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,6 +11,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
